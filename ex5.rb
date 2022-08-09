03ef00f6597ae0f6a7646183d9a0c39c229ea5f4
Niki J <niki.jiandani@gmail.com>
2022-08-08 22:05:30 -0400

    First 10 exercises

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Niki Jiandani'
+my_age = 31
+my_height = 74 #inches
+my_weight = 180 #lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}"
+puts "She's #{my_height} inches tall."
+puts "She's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "She's got #{my_eyes} eyes and #{my_hair} hair."
+puts "Her teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
