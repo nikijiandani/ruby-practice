a5f927f5fe4d44763e73c7d213bbd55369fbbb48
Niki J <niki.jiandani@gmail.com>
2022-08-10 18:55:52 -0400

    Add ex 13

---
+++ ex13.rb
@@ -0,0 +1,11 @@
+first, second, third = ARGV
+
+puts "Your first variable is #{first}"
+
+puts "Your second variable is #{second}"
+
+puts "Your third variable is #{third}"
+
+puts "What is your favourite color?"
+input = $stdin.gets.chomp
+print "#{input} is awesome!"
